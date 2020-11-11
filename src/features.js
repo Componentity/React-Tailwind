d2c2456fb34a28f4910931bae06ae333e6484f8a
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2020-11-11 22:56:03 +0430

    New Features List

---
+++ src/features.js
@@ -0,0 +1,26 @@
+//NOTE! List of Features Completed
+
+//. 01: Modal
+//. 02: Alert
+//. 03: Tooltip
+//. 03: Collapse
+//. 04: Drawer
+
+//NOTE! List of New Features
+//. 01: Badges
+//. 02: Breadcrumb
+//. 03: Buttons
+//. 04: Buttons Group
+//. 05: Cards
+//. 06: Carousel
+//. 07: Dropdown
+//. 08: Forms
+//. 09: Input Group
+//. 10: List Group
+//. 11: Media Objects
+//. 12: Navs
+//. 13: Navbars
+//. 14: Pagination
+//. 15: Progress Bar
+//. 16: Spinners (Animation)
+//. 17: Toast
